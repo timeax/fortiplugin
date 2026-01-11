9891eec0c3154f6cf22e91989627a210ed3a082c
David Okpako <105345335+timeax@users.noreply.github.com>
2026-01-11 01:30:23 +0100

    update

--- src/Permissions/Evaluation/PermissionService.php
+++ src/Permissions/Evaluation/PermissionService.php
@@ -34,7 +34,7 @@ use Timeax\FortiPlugin\Permissions\Registry\PermissionRegistry;
 
 final readonly class PermissionService implements PermissionServiceInterface
 {
-    use PermissionServiceAllowTrait, PermissionServiceUpsertTrait, PermissionServiceListTrait;
+    use PermissionServiceDecisionTrait, PermissionServiceUpsertTrait, PermissionServiceListTrait;
 
     public function __construct(
         private PermissionRepositoryInterface $repo,
